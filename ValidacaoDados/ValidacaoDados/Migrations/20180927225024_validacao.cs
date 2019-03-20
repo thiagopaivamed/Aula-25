865ba8ac72107c554a114fbd2558746660b7211a
Thiago Paiva  Medeiros <thiagopaivamed@gmail.com>
2019-03-19 21:37:58 -0300

    Validação de dados com Data Annotations

---
+++ ValidacaoDados/ValidacaoDados/Migrations/20180927225024_validacao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ValidacaoDados.Migrations
+{
+    public partial class validacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    PessoaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Idade = table.Column<int>(nullable: false),
+                    Nascimento = table.Column<DateTime>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    ConfirmarEmail = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.PessoaId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
---
+++ ValidacaoDados/ValidacaoDados/Models/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ValidacaoDados.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+
+        [Required(ErrorMessage ="Campo obrigatório")]
+        [StringLength(20, ErrorMessage = "Use menos caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [Range(1,90, ErrorMessage ="Você não é velho assim ! Digite um valor válido")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [DataType(DataType.Date)]
+        public DateTime Nascimento { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(50, ErrorMessage = "Use menos caracteres")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(50, ErrorMessage = "Use menos caracteres")]
+        [Compare("Email")]
+        public string ConfirmarEmail { get; set; }
+
+    }
+}
